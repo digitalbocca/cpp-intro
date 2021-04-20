91c0e05f982b7e0dd14dfc5c834a76a5828f6961
Gabriel Bertola Bocca <digitalbocca@gmail.com>
2021-04-20 10:19:39 -0300

    Aprendendo strings

---
+++ variables-strings.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main () {
+  int a = 2;
+  int b = 2;
+
+  cout << a + b << endl;
+
+  string message = "Esta é a mensagem da string.";
+
+  cout << message << endl;
+
+  return 0;
+}
+
